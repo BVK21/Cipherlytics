66631e4b1ef14831e5e1d1705b1e2d690cd039bf
bvk21 <bvk21@fsu.edu>
2024-09-24 22:44:39 -0500

    Project Setup day 2

--- Cipherlytics/src/main/java/CipherlyticsService.java
+++ Cipherlytics/src/main/java/CipherlyticsService.java
@@ -17,4 +17,11 @@ public class CipherlyticsService {
     public Response sayHello() {
         return Response.ok("Hello, World! Bhim").build();
     }
+
+    @GET
+    @Path("/hello/{name}")
+    @Produces("text/plain")
+    public Response sayHello(@PathParam("name") String name) {
+        return Response.ok("Hello, " + name + "!").build();
+    }
 }
